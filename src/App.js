771d48a74a095bf08f8ee17dc0286a31b713e291
Silviu <silviu074@gmail.com>
2023-04-05 22:55:47 +0300

    Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import axios from 'axios'
+import React, { useState, useEffect} from 'react'
+
+
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [cityInfo, setCityInfo] = useState()
+
+  // API request
+
+  useEffect( () => {
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=621f1f003c7991f2765209be469931c2')
+      .then(response => {
+          setLoading(false)
+          setCityInfo(response.data)
+          setError(false)
+      })
+      .catch(err => {
+          setLoading(false)
+          setCityInfo({})
+          setError(true)
+          console.log(err)
+      })
+  }, [])
+
+  console.log(cityInfo)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? 'Loading' : null}
+      {error ? 'Something went wrong' : null}
+      <h1>Weather app</h1>
     </div>
   );
 }
